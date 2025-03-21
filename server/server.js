2dd34db047a9711051d88d54015737c6fc5a37ac
Benin-RH <128675525+Benin-RH@users.noreply.github.com>
2025-03-21 22:22:55 +0530

    cros and vercel managed

--- server/server.js
+++ server/server.js
@@ -3,12 +3,13 @@ const cors = require('cors')
 const routes = require("./Router.js");
 const db = require("./dbConnection.js")
 const PORT = 5000;
-
+const cors = require('cors')
 const app = express();
 app.use(express.json());
 app.use(cors());
 app.use("/", routes);
-
+app.use(cors({ origin: "https://your-frontend.vercel.app" }));
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
 });
+
